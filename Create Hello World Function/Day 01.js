99020d0d5caa1f60010d32b738cb75771d62fac8
PavithMadushan <pavithmadushan@gmail.com>
2023-10-27 19:28:01 +0530

    Create Hello World Function

---
+++ Create Hello World Function/Day 01.js
@@ -0,0 +1,18 @@
+
+//  @return {Function}
+ 
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+        
+    }
+};
+
+const a=createHelloWorld();
+console.log(a)
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
